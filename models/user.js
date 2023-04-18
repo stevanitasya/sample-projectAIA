320b8a73f85a334dd29893a7ec04718b181ade03
Stevani Natasya Sero <72642424+stevanitasya@users.noreply.github.com>
2023-04-18 10:39:08 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     Fullname: {
         type: String,
         required: true,
@@ -15,4 +15,4 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('user', userSchema);
