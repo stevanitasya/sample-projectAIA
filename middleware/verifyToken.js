aba0f70a175e252891c2e868f84caa8361ce98be
Stevani Natasya Sero <72642424+stevanitasya@users.noreply.github.com>
2023-04-27 11:25:03 +0800

    Update verifyToken.js

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -1,29 +1,37 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res, next) => {
-	let token = req.headers.authorization;
-	if (!token) {
-		return res.status(403).json({
-			message: "No Token found!",
-		});
-	}
+const checkToken = (req, res, next) => {
+    let token = req.headers.authorization;
 
-	try {
-		const jwtPayload = jwt.verify(token, "secret_code");
-		if (!jwtPayload) {
-			return res.status(403).json({
-				message: "No user found!",
-			});
-		}
-		res.user = {
-			email: jwtPayload.email,
-		};
-		next();
-	} catch (error) {
-		return res.status(500).json({
-			message: error,
-		});
-	}
-};
+    if(!token) {
+        return res.status(403).json({
+            message: 'unauthentication'
+        })
+    }
 
-module.exports = verifyToken;
+    if(token.toLowerCase().startsWith('bearer')) {
+        token = token.slice('bearer'.length).trim()
+    }
+
+    try {
+        const jwtPayload = jwt.verify(token, 'secret_key');
+
+        if(!jwtPayload) {
+            return res.status(403).json({
+                message: 'huss go away'
+            })
+        }
+
+        res.user = {
+            id: jwtPayload
+        }
+
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            message: error
+        })
+    }
+}
+
+module.exports = checkToken;
